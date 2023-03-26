a57c999e755e8cd427177659fd5584daad8bf1ae
Sadhna singh <sadhna.singh.work@gmail.com>
2023-03-26 14:07:40 +1300

    updated RegisterCardForm component to store the input with useState hook and log it to console on submit

--- frontend-test/src/components/RegisterCardForm.tsx
+++ frontend-test/src/components/RegisterCardForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 
 //Define interface for all form values 
@@ -12,21 +12,44 @@ interface FormValues {
 
 
 const RegisterCardForm: React.FC= () => {
-  
+
+  //using useState hook to store FormValues
+
+  const [formValues, setFormValues] = useState<FormValues>({
+    creditCard:0,
+    cvv:0,
+    expiryDate:''
+  })
+
+  //function to handle form submission
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formValues)
+  }
+
+  //function to handle form input changes
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]:value})
+  }
   return (
     
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="creditCard">Credit Card</label>
-          <input type="number" id="creditCard" name="creditCard" placeholder="Credit Card Number" />
+          <input type="number" id="creditCard" name="creditCard" placeholder="Credit Card Number" 
+          value={formValues.creditCard } 
+          onChange={handleChange}/>
         </div>
         <div>
         <label htmlFor="cvv">CVV</label>
-        <input type="number" id="cvv" name="cvv" placeholder="CVV" />
+        <input type="number" id="cvv" name="cvv" placeholder="CVV" value={formValues.cvv} 
+        onChange={handleChange}/>
         </div>
         <div>
         <label htmlFor="expiryDate">Expiry Date</label>
-        <input type="text" id="expiryDate" name="expiryDate" placeholder="Expiry Date" />
+        <input type="date" id="expiryDate" name="expiryDate" placeholder="Expiry Date" value={formValues.expiryDate} 
+        onChange={handleChange}/>
         </div>
         <button type="submit">Submit</button>
       </form>
