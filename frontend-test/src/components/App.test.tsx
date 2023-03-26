1a546baed68591880e092cf92ecc9082e42cb1b3
Sadhna singh <sadhna.singh.work@gmail.com>
2023-03-26 16:53:29 +1300

    added jest-dom library and basic test

--- frontend-test/src/components/App.test.tsx
+++ frontend-test/src/components/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders the App page', () => {
+  render(
+    
+        <App />
+     
+  )
+  expect(screen.getByText('Register Card Form')).toBeInTheDocument()
+})
