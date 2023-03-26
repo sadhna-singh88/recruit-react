a9e979688ff09a180c289cdd91271e32b50a0fe0
Sadhna singh <sadhna.singh.work@gmail.com>
2023-03-26 17:04:37 +1300

    added another test for registration form

--- frontend-test/src/components/App.test.tsx
+++ frontend-test/src/components/App.test.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
 import '@testing-library/jest-dom'
+import RegisterCardForm from './RegisterCardForm';
 
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 test('renders the App page', () => {
   render(
@@ -17,4 +13,17 @@ test('renders the App page', () => {
      
   )
   expect(screen.getByText('Register Card Form')).toBeInTheDocument()
+})
+
+
+
+test('renders the RegisterCardForm page', () => {
+  //test for all input fields
+  render(
+    <RegisterCardForm/>
+  )
+  expect(screen.getByPlaceholderText('Credit Card Number')).toBeInTheDocument()
+  expect(screen.getByPlaceholderText('CVC')).toBeInTheDocument()
+  expect(screen.getByPlaceholderText('Expiry Date')).toBeInTheDocument()
+  expect(screen.getByText('Submit')).toBeInTheDocument()
 })
