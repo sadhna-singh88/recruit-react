29456d09aa041be06e19b038d61247e4ea9040ce
Sadhna singh <sadhna.singh.work@gmail.com>
2023-03-26 14:37:45 +1300

    edit validation in form and code

--- frontend-test/src/components/RegisterCardForm.tsx
+++ frontend-test/src/components/RegisterCardForm.tsx
@@ -8,9 +8,6 @@ interface FormValues {
   expiryDate:string;
 }
 
-
-
-
 const RegisterCardForm: React.FC= () => {
 
   //using useState hook to store FormValues
@@ -24,7 +21,10 @@ const RegisterCardForm: React.FC= () => {
   //function to handle form submission
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    //validate form input
+    if(validateInput()){
     console.log(formValues)
+    }
   }
 
   //function to handle form input changes
@@ -32,6 +32,31 @@ const RegisterCardForm: React.FC= () => {
     const {name, value} = e.target;
     setFormValues({...formValues, [name]:value})
   }
+
+//Creating a input validation funtion for form 
+  const validateInput = () => {
+    const {creditCard, cvv, expiryDate} = formValues
+    let isValid = true;
+    // credit card validation
+    if (isNaN (creditCard)) {
+      isValid = false;
+      console.log('Credit card number is not a number')
+    }
+    // cvv validation
+    if (isNaN (cvv)) {
+      isValid = false;
+      console.log('CVV is not a number')
+    }
+    // expiry date validation
+    if (isNaN (Date.parse(expiryDate))) {
+      isValid = false;
+      console.log('Expiry date is not a valid date')
+    }
+    return isValid;
+  }
+    
+
+
   return (
     
       <form onSubmit={handleSubmit}>
@@ -39,17 +64,17 @@ const RegisterCardForm: React.FC= () => {
           <label htmlFor="creditCard">Credit Card</label>
           <input type="number" id="creditCard" name="creditCard" placeholder="Credit Card Number" 
           value={formValues.creditCard } 
-          onChange={handleChange}/>
+          onChange={handleChange} required/>
         </div>
         <div>
         <label htmlFor="cvv">CVV</label>
         <input type="number" id="cvv" name="cvv" placeholder="CVV" value={formValues.cvv} 
-        onChange={handleChange}/>
+        onChange={handleChange} required/>
         </div>
         <div>
         <label htmlFor="expiryDate">Expiry Date</label>
         <input type="date" id="expiryDate" name="expiryDate" placeholder="Expiry Date" value={formValues.expiryDate} 
-        onChange={handleChange}/>
+        onChange={handleChange} required/>
         </div>
         <button type="submit">Submit</button>
       </form>
